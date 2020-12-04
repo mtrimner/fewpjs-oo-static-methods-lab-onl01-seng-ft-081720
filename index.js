18478f4778502d75d08d19bb068b6cd8c2298e23
mtrimner <matt.trimner@gmail.com>
2020-12-03 17:33:16 -0800

    commit tree ff63cf6998ea5e072a73f64a0ff5a12ff2efeac2

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(sentence) {
+    let exemptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newSentence = []
+    let sentenceArray = sentence.split(" ")
+    for ( let i = 0; i < sentenceArray.length; i++)
+      if (i == 0) {
+        newSentence.push(this.capitalize(sentenceArray[i]))
+      } else {
+        if (exemptWords.includes(sentenceArray[i])) {
+          newSentence.push(sentenceArray[i])
+        } else {
+          newSentence.push(this.capitalize(sentenceArray[i]))
+        }
+      }
+      return newSentence.join(" ")
+  }
 }
